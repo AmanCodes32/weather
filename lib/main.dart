832902568c14bc45ae8fbc5dc54dee976da49ba2
Aman Rupareliya <amanrupareliya@thirtysixxmm>
2022-03-04 22:01:11 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
