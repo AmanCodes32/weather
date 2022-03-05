11c095ccee3d3c091b5d65d633565dc2195b66f7
Aman Rupareliya <amanrupareliya@thirtysixxmm>
2022-03-05 23:18:10 +0530

    done with changing cities

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
 import 'package:weather_practise_app/network/location.dart';
 import 'package:weather_practise_app/service/weather.dart';
+import 'package:weather_practise_app/settings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,29 +33,29 @@ class _MyHomePageState extends State<MyHomePage> {
   var data;
   var address;
   bool _isLoading = false;
+  var icon = '';
+  String url = '';
+  GlobalKey<FormState> key = GlobalKey();
+  TextEditingController city = TextEditingController();
+
+  void getCity() async {
+    data = await getData(address);
+    icon = data['weather'][0]['icon'];
+    url = 'http://openweathermap.org/img/wn/$icon@2x.png';
+    print(data);
+    _isLoading = false;
+    setState(() {});
+  }
+
   @override
   void initState() {
     _isLoading = true;
     Future.delayed(Duration.zero).then((value) async {
       Position position = await determinePosition();
-      print(position);
       address = await getAddress(position);
-      data = await getData(position.latitude, position.longitude);
-      print(data);
-      _isLoading = false;
-      setState(() {});
+      getCity();
     });
     super.initState();
-  }
-
-  String getIcon(String description) {
-    String url = '';
-    if (description == "CLEAR SKY") {
-      url = "assets/icons/wi-day-sunny.svg";
-    } else if (description == "CLOUDY") {
-      url = "assets/icons/wi-cloud.svg";
-    }
-    return url;
   }
 
   @override
@@ -70,11 +71,64 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         elevation: 0,
         actions: [
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            onPressed: () {},
-            color: Colors.grey,
-          )
+          PopupMenuButton(
+            initialValue: 0,
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: 1,
+                  child: Text('Change City'),
+                ),
+                PopupMenuItem(
+                  value: 2,
+                  child: Text('Settings'),
+                ),
+              ];
+            },
+            onSelected: (value) {
+              if (value == 1) {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Change City'),
+                        content: Form(
+                          key: key,
+                          child: TextFormField(
+                            controller: city,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'City cannot be empty';
+                              }
+                              key.currentState!.save();
+                              address = city.text;
+                              setState(() {});
+                            },
+                            decoration: InputDecoration(hintText: 'Enter City'),
+                          ),
+                        ),
+                        actions: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.black),
+                              onPressed: () {
+                                if (key.currentState!.validate()) {
+                                  Navigator.of(context).pop();
+                                  _isLoading = true;
+                                  getCity();
+                                }
+                              },
+                              child: Text('Change'))
+                        ],
+                      );
+                    });
+              } else {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SettingsScreen()));
+              }
+            },
+            icon: Icon(Icons.more_vert, color: Colors.grey),
+          ),
         ],
         backgroundColor: Colors.white,
       ),
@@ -83,48 +137,126 @@ class _MyHomePageState extends State<MyHomePage> {
               child: CircularProgressIndicator(
               color: Colors.black,
             ))
-          : Container(
-              height: height,
-              width: width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    address.toString(),
-                    style: const TextStyle(fontSize: 28, color: Colors.black),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    data['current']['weather'][0]['description']
-                        .toString()
-                        .toUpperCase(),
-                    style: TextStyle(fontSize: 24, letterSpacing: 4),
-                  ),
-                  SizedBox(height: 20),
-                  SvgPicture.asset(
-                    getIcon(data['current']['weather'][0]['description']
-                        .toString()
-                        .toUpperCase()),
-                    height: 80,
-                    width: 80,
-                  ),
-                  SizedBox(height: 20),
-                  Text.rich(TextSpan(
-                      text: data['current']['temp'].toString(),
-                      style: TextStyle(fontSize: 80),
-                      children: [TextSpan(text: '°')])),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      buildColumn('max', data['daily'][0]['temp']['max']),
-                      buildColumn('min', data['daily'][0]['temp']['min'])
-                    ],
-                  ),
-                  Row(
-                    children: [],
-                  )
-                ],
+          : SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: width * 0.05),
+                height: height,
+                width: width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: height * 0.1),
+                    Text(
+                      data['name'],
+                      style: const TextStyle(fontSize: 28, color: Colors.black),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      data['weather'][0]['description']
+                          .toString()
+                          .toUpperCase(),
+                      style: TextStyle(fontSize: 24, letterSpacing: 4),
+                    ),
+                    SizedBox(height: 20),
+                    Image.network(url),
+                    SizedBox(height: 20),
+                    Text.rich(TextSpan(
+                        text: data['main']['temp'].toString().substring(
+                            0, data['main']['temp'].toString().indexOf('.')),
+                        style: TextStyle(fontSize: 100),
+                        children: [TextSpan(text: '°')])),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        buildColumn('max', data['main']['temp_max']),
+                        VerticalDividerWidget(),
+                        buildColumn('min', data['main']['temp_min'])
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Divider(
+                      color: Colors.grey,
+                      endIndent: 20,
+                      indent: 20,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'wind speed',
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 20),
+                            ),
+                            SizedBox(height: 10),
+                            Text(data['wind']['speed'].toString() + ' m/s',
+                                style: TextStyle(fontSize: 16))
+                          ],
+                        ),
+                        VerticalDividerWidget(),
+                        Column(
+                          children: [
+                            Text(
+                              'sunrise',
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 20),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                                DateFormat.Hm()
+                                        .format(
+                                            DateTime.fromMillisecondsSinceEpoch(
+                                                data['sys']['sunrise']))
+                                        .toString() +
+                                    ' AM',
+                                style: TextStyle(fontSize: 16))
+                          ],
+                        ),
+                        VerticalDividerWidget(),
+                        Column(
+                          children: [
+                            Text(
+                              'sunset',
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 20),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                                DateFormat.Hm()
+                                        .format(
+                                            DateTime.fromMillisecondsSinceEpoch(
+                                                data['sys']['sunset']))
+                                        .toString() +
+                                    ' PM',
+                                style: TextStyle(fontSize: 16)),
+                          ],
+                        ),
+                        VerticalDividerWidget(),
+                        Column(
+                          children: [
+                            Text(
+                              'humidity',
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 20),
+                            ),
+                            SizedBox(height: 10),
+                            Text(data['main']['humidity'].toString() + ' %',
+                                style: TextStyle(fontSize: 16))
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
     );
@@ -141,6 +273,26 @@ class _MyHomePageState extends State<MyHomePage> {
         Text.rich(
             TextSpan(text: data.toString(), children: [TextSpan(text: '°')]))
       ],
+    );
+  }
+}
+
+class VerticalDividerWidget extends StatelessWidget {
+  const VerticalDividerWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 35,
+      child: VerticalDivider(
+        color: Colors.grey,
+        thickness: 2,
+        indent: 5,
+        endIndent: 0,
+        width: 20,
+      ),
     );
   }
 }
